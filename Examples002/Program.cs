a8408e0591eaa037c4b2a94df2f7bb252760d187
Alexandr <i_am_jeweller@vk.com>
2021-12-18 18:10:11 +0300

    Initial commit

---
+++ Examples002/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите ваше имя ");
+String userName = Console.ReadLine();
+Console.Write("Привет, ");
+Console.WriteLine(userName);
---
+++ Examples005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+String ueserName = Console.ReadLine();
+
+if (ueserName.ToLower() == "маша") {
+     Console.WriteLine("Ура, это Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(ueserName);
+}
